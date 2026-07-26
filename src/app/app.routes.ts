14d0530471a0edbb0adf2670508415912ae293d6
rohan-K210 <rohankrishna8a@gmail.com>
2026-07-26 11:16:25 +0530

    edit option fixed for header actions in deals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -83,7 +83,7 @@ export const routes: Routes = [
       {
         path: 'deals/:id',
         loadComponent: () =>
-          import('orque-ui').then(m => m.RecordDetailComponent),
+          import('../app/features/detail-page/detail-page').then(m => m.CrmDetailComponent),
         data: { resource: 'deals' }
       },
       {
